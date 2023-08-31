bb6ae90189bd96823b0de495ef54391047c9982f
Denilson gomes da Conceicao <121492709+Danin07@users.noreply.github.com>
2023-09-01 01:47:30 +0300

    Add files via upload

---
+++ 1-redirecionar/public/index.php
@@ -0,0 +1,99 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] ==="POST"){
+
+    header("location: des.php");
+
+exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redicrecionamento de pagina php</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
+<div class="head">
+    <ul>
+        <li>HOME</li>
+        <li>CONTACT</li>
+        <li>HOME</li>
+    </ul>
+</div>
+
+
+
+<div class="contain">
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+
+</div>
+
+
+
+<div class="contain">
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+    <div class="card">
+        <img src="/imagens/Meat-PNG-Download-Image.png" alt="">
+    </div>
+
+
+</div>
+
+<form action="" method="post">
+    <label for="">name
+        <input type="text" name="" id="">
+    </label>
+
+    <label for="">aplido
+        <input type="text" name="" id="">
+    </label>
+
+<button type="submit">seno</button>
+
+
+</form>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
